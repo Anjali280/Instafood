5064c8e3fe38d1dd19cb058b4c53f6378137a7d6
Anjali280 <mondalanjali0707@gmail.com>
2023-04-03 04:34:21 +0530

    Carousel and footer component added

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -1,17 +1,16 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Card from "../components/Card";
+import Carousel from "../components/Carousel";
 
 const Home = () => {
   return (
     <div>
-      <div>
-        <Navbar />
-      </div>
-      <div>Body</div>
-      <div>
-        <Footer />
-      </div>
+      <Navbar />
+      <Carousel />
+      <Card />
+      <Footer />
     </div>
   );
 };
