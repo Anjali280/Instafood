fba333844643bf6d54890a2c6802bec3d0ca72e5
Anjali280 <mondalanjali0707@gmail.com>
2023-04-05 22:14:49 +0530

    Products displayed in the card

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -15,6 +15,7 @@ const Home = () => {
     //console.log(response.payload.findFood_item);
     setFoodItems(response.payload.findFood_item);
     setFoodCat(response.payload.findfoodCategory);
+    console.log(foodItems);
   };
 
   useEffect(() => {
@@ -24,21 +25,46 @@ const Home = () => {
     <div>
       <Navbar />
       <Carousel />
-      <div className="m-3">
+      <div className="container">
         {foodCat !== []
           ? foodCat.map((data) => {
               return (
-                <div>
+                <div className="row mb-3">
                   <div key={data._id} className="fs-3 m-3">
-                    {data.categoryName}
+                    {data.CategoryName}
                   </div>
                   <hr />
-                  {foodItems}
+                  {foodItems !== [] ? (
+                    foodItems
+                      .filter(
+                        (items) => items.CategoryName === data.CategoryName
+                        //  &&
+                        // items.name
+                        //   .toLowerCase()
+                        //   .includes(search.toLowerCase())
+                      )
+                      .map((element) => {
+                        return (
+                          <div
+                            key={element.id}
+                            className="col-12 col-md-6 col-lg-3"
+                          >
+                            <Card
+                              foodName={element.name}
+                              item={element}
+                              options={element.options[0]}
+                              ImgSrc={element.img}
+                            />
+                          </div>
+                        );
+                      })
+                  ) : (
+                    <div> No Such Data </div>
+                  )}
                 </div>
               );
             })
           : ""}
-        <Card />
       </div>
       <Footer />
     </div>
